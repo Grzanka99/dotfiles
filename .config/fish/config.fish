2fdf1a0b7a4e0c2b1719c1056c8b7c4894eea2bb
Cezary Grzanka <cezarygrzanka@gmail.com>
2020-06-06 23:35:33 +0200

    Add config fish, update alacritty font size (is small, bcs my weir scale)

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+alias gs="git status"
+alias gc="git commit"
+alias ls="exa --long --header --git"
+alias battery="upower -i /org/freedesktop/UPower/devices/battery_BAT0"
+alias pacrem="sudo pacman -Rns"
+alias cpu="cpupower-gui"
+alias db="mysql -u cezary -p" 
+
+source ~/.nvm-fish/nvm.fish
