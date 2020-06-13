d62e1367ffd8d5a8f85be55e170db1189bc6535c
Cezary Grzanka <cezarygrzanka@gmail.com>
2020-06-14 00:35:54 +0200

    Some important updates - touchpad config, throttled config, file with base apps

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ alias ls="exa --long --header --git"
 alias battery="upower -i /org/freedesktop/UPower/devices/battery_BAT0"
 alias pacrem="sudo pacman -Rns"
 alias cpu="cpupower-gui"
-alias db="mysql -u cezary -p" 
+alias db="mysql -u cezary -p"
+alias show="kak -ro"
 
 source ~/.nvm-fish/nvm.fish
