88a99f936dccbbe7b47f5e827bf73ea6c492c247
Cezary Grzanka <cezarygrzanka@gmail.com>
2020-08-04 15:16:18 +0200

    Some big changes in WM configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar cezary >>/tmp/polybar1.log 2>&1 &
+polybar cezary -c $HOME/.config/polybar/config-$1 >>/tmp/polybar1.log 2>&1 &
 
 echo "Bars launched..."
